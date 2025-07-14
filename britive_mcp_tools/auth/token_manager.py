efb9240ff2021174f720d27a310bd3aafd73b701
ShreerajBritive <shreeraj.mhatre@britive.com>
2025-07-14 09:16:58 +0000

    Adds the option of Static token

--- britive_mcp_tools/auth/token_manager.py
+++ britive_mcp_tools/auth/token_manager.py
@@ -13,13 +13,16 @@ class TokenManager:
         self.read_token()
 
     def read_token(self):
-        self.config.read(self.token_file)
-        try:
-            self.token = self.config[self.tenant]["accessToken"]
-            self.expiry = int(self.config[self.tenant].get("safeExpirationTime", "0"))
-        except KeyError:
-            self.token = None
-            self.expiry = 0
+        if os.getenv("BRITIVE_STATIC_TOKEN"):
+            self.token = os.getenv("BRITIVE_STATIC_TOKEN")
+        else:
+            self.config.read(self.token_file)
+            try:
+                self.token = self.config[self.tenant]["accessToken"]
+                self.expiry = int(self.config[self.tenant].get("safeExpirationTime", "0"))
+            except KeyError:
+                self.token = None
+                self.expiry = 0
 
     def is_expired(self):
         return not self.token or (time.time() + self.token_expiry_buffer) > self.expiry
@@ -34,6 +37,10 @@ class TokenManager:
         self.read_token()
 
     def get_token(self):
+        britive_static_token = os.getenv("BRITIVE_STATIC_TOKEN")
+        if britive_static_token:
+            self.token = britive_static_token
+            return self.token
         if self.is_expired() or self.is_logged_out():
             self.refresh_token()
         return self.token
